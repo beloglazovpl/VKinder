8e4d73b4d42699d14854e24561758ac8e088ce8f
Pavel Beloglazov <beloglazov_pl@icloud.com>
2022-02-01 22:16:11 +1000

    Add README.md file

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 token = 'c772478adf7bfa28d36de7f2f90e1121999e2ca4726a8bac91b21a41005cbe26141aff6915bedcc1459a1'
-token_user = 'b3d0d9e03099c61f566e2cc7c11a08f54145cdf7271531081d88bda88c6353ad216d401773cbcdfeb1796'
+
+'''
+Перед использованием программы ведите свой token_user
+'''
+
+token_user = '...'
